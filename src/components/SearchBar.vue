a6188e360584643e3f22d6f382f7f1752d137729
ghigbie <gehigbie@gmail.com>
2018-07-18 19:21:21 -0600

    Get working search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar' //This needs to be a string!!!
+};
+</script>
+
+<style>
+
+</style>
+
