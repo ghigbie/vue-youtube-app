aaf1a911cb519406f31efb5eb93d7ae2a4950365
ghigbie <gehigbie@gmail.com>
2018-07-19 14:14:33 -0600

    Pass video prop to video list item

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,17 @@
 <template>
     <ul>
-        VideoList
-        {{ videos.length + 2344 }}
+        <VideoListItem v-for="video in videos"></VideoListItem>
     </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
     name: 'VideoList',
+    components: {
+        VideoListItem
+    },
     props: {//You have to list out all of the props this component expects to see
         videos: Array //This is the data type that is expected
     }
--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,9 +1,12 @@
 <template>
-    
+    <li>
+        Video List Item
+    </li>
 </template>
 
 <script>
 export default {
-    
-}
+   name: 'VideoListItem',
+   props: ['video']
+};
 </script>
