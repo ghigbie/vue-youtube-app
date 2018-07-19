f45644e873df1db1162d832f1bb9ad78ddc2ec5f
ghigbie <gehigbie@gmail.com>
2018-07-18 19:07:21 -0600

    Change el to

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import vue from 'vue';
 import App from './App';
 
 new Vue ({
-    el: '#app',
-    render: h => h(App), //h is an abreviation for createElement
-});
+    render: h => h(App) //h is an abreviation for createElement
+}).$mount('#app');
