e0be4388c9edd7ba09b23760c013461e7c915c3a
ghigbie <gehigbie@gmail.com>
2018-07-19 19:17:03 -0600

    Add image to display

--- src/components/VideoDisplay.vue
+++ src/components/VideoDisplay.vue
@@ -1,14 +1,22 @@
 <template>
     <div v-if="video">
-        <img :src="displayImgURL" />
-        <h2>{{ video.snippet.title }}</h2>
+        <img :src="displayImageURL" />
+        <h2>{{ displayTitle }}</h2>
     </div>
 </template>
 
 <script>
 export default {
     name: 'VideoDisplay',
-    props: ['video']
+    props: ['video'],
+    computed: {
+        displayImageURL(){
+            return this.video.snippet.thumbnails.high.url;
+        },
+        displayTitle(){
+            return this.video.snippet.title;
+        }
+    }
 };
 </script>
 
